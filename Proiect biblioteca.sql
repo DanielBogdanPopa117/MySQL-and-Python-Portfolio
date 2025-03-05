354e9ee01777ef5b1764a7c1fde2868e02255a24
DanielBogdanPopa117 <bogdanpopa117@gmail.com>
2025-03-05 17:45:25 +0200

    Update Proiect biblioteca.sql

--- Proiect biblioteca.sql
+++ Proiect biblioteca.sql
@@ -130,3 +130,129 @@ WHERE id IN (SELECT carte_id FROM imprumuturi);
 SELECT DISTINCT cititor
 FROM imprumuturi
 WHERE carte_id IN (SELECT id FROM carti WHERE gen = 'Filozofie');
+
+#gasim toate cartile scrise de Thomas Mann
+SELECT titlu, nume FROM carti
+JOIN autori
+ON autori.id = carti.autor_id 
+WHERE nume='Thomas Mann';
+
+#gasim utilizatorii care au imprumutat carti in ultimele 30 de zile
+SELECT DISTINCT cititor FROM imprumuturi
+WHERE data_imprumut >= CURDATE() - INTERVAL 30 DAY; 
+
+#calculam durata medie a unui imprumut
+SELECT AVG(DATEDIFF(data_returnare, data_imprumut)) AS durata_medie_zile
+ FROM imprumuturi
+ WHERE data_returnare IS NOT NULL;
+ 
+ #gasim autorul cu cele mai multe carti in biblioteca
+ SELECT titlu FROM carti
+ WHERE autor_id = (
+ SELECT autor_id FROM carti
+ GROUP BY autor_id
+ ORDER BY COUNT(*) DESC
+ LIMIT 1);
+ 
+ #afisam cititorii care au imprumutat cea mai recenta carte adaugata in biblioteca
+ SELECT cititori FROM iprumuturi
+ WHERE carte_id = (
+ SELECT id FROM carti
+ ORDER BY data_publicarii DESC
+ Limit 1
+ );
+ 
+ #gasim cartea cu cel mai lung titlu din biblioteca
+ SELECT titlu FROM carti
+ WHERE LENGTH(titlu) = (
+ SELECT MAX(LENGTH(titlu)) FROM carti
+ );
+ 
+ #gasim toate cartile care au fost imprumutate de cel putin doi cititori diferiti
+ SELECT titlu FROM carti
+ WHERE id IN (
+ SELECT carte_id FROM imprumuturi
+ GROUP BY carte_id
+ HAVING COUNT(DISTINCT cititor) >= 2
+ );
+ 
+ #titlurile si autorii cartilor imprumutate cel mai recent
+ SELECT carti.titlu, autor.nume FROM carti
+ JOIN autori
+ ON carti.autor_id = autori.id
+ WHERE carti.id = (
+ SELECT carte_id FROM imprumuturi
+ ORDER by data_imprumut DESC
+ LIMIT 1
+ );
+ 
+ #gasim cititorul care a imprumutat cele mai multe carti
+ SELECT cititor FROM imprumuturi
+ GROUP BY cititor
+ ORDER BY COUNT(*) DESC
+ LIMIT 1;
+ 
+ #afisam cartile care nu au fost niciodata imprumutate
+ SELECT titlu FROM carti
+ WHERE id NOT IN (SELECT carte_id FROM imprumuturi);
+ 
+ #gasim autorul care are cele mai multe carti imprumutate
+ SELECT nume FROM autori
+ WHERE id = (
+ SELECT carti.autor_id FROM carti
+ JOIN IMPRUMUTURI ON carti.id = imprumuturi.carte_id
+ GROUP BY carti.autor_id
+ ORDER BY COUNT(*) DESC
+ LIMIT 1
+ );
+ 
+ #gasim toate cartile care nu au fost inca returnate
+ SELECT titlu FROM carti
+ WHERE id IN ( 
+	SELECT carte_id FROM imprumuturi
+    WHERE data_returnare IS NULL OR data_returnare> CURDATE()
+    );
+    
+    #afisam cititorii care au imprumutat carti scrise de cel putin doi autori diferiti
+SELECT cititor from imprumuturi
+WHERE carte_id IN (SELECT id FROM carti)
+GROUP BY cititor
+HAVING COUNT(DISTINCT (SELECT autor_id FROM carti WHERE carti.id = imprumuturi.carte_id)) >= 2;
+
+#gasim genul de carte care a fost imprumutat cel mai des
+SELECT gen FROM carti
+WHERE id = (
+	SELECT carte_id FROM imprumuturi
+    GROUP BY carte_id
+    ORDER BY COUNT(*) DESC
+    Limit 1
+    );
+
+#afisam autorul cu cele mai vechi carti din biblioteca
+SELECT nume FROM autori
+WHERE id = (
+SELECT autor_id FROM carti
+ORDER BY data_publicarii ASC
+Limit 1
+);
+
+#calculam durata medie de imprumut pentru fiecare gen de carte
+SELECT carti.gen, AVG(DATEDIFF(imprumuturi.data_returnare, imprumuturi.data_imprumut)) AS durata_medie
+FROM imprumuturi
+JOIN carti
+ON carti.id = imprumuturi.carte_id
+WHERE imprumuturi.data_returnare IS NOT NULL
+GROUP BY carti.gen;
+
+#gasim cititorii care au returnat toate cartile imprumutate
+SELECT cititor FROM imprumuturi
+GROUP BY cititor
+HAVING COUNT(*) = SUM(CASE WHEN data_returnare IS NOT NULL THEN 1 ELSE 0 END);
+
+#listam toate cartile care au fost imprumutate de toti cititorii care au facut cel putin un imprumut
+SELECT titlu FROM carti
+WHERE id IN (
+    SELECT carte_id FROM imprumuturi
+    GROUP BY carte_id
+    HAVING COUNT(DISTINCT cititor) = (SELECT COUNT(DISTINCT cititor) FROM imprumuturi)
+);
